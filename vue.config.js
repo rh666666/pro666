64e006d9cafe2da67401681d4e91bda61913d945
rh666666 <787314057@qq.com>
2025-03-06 16:02:16 +0800

    重构 vue.config.js，移除代理配置并启用依赖转译

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,6 @@
 // vue.config.js
-module.exports = {
-  // 不需要代理到后端，关闭代理配置
-  devServer: {
-    proxy: {
-      // 删除或注释掉代理配置，避免请求转发到真实后端
-      // '/api': {
-      //   target: 'http://localhost:5000', // 后端服务地址
-      //   changeOrigin: true,
-      //   secure: false,
-      // },
-    },
-  },
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false
+})
